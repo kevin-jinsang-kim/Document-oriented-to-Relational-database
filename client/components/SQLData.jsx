aeabfbc9992072e02c1aef7aab0b4b6d908e500f
Kevin Kim <hwaksiliya@hotmail.com>
2020-04-06 10:50:50 +1200

    sql tables attempt on frontend

--- client/components/SQLData.jsx
+++ client/components/SQLData.jsx
@@ -8,6 +8,30 @@ class SQLData extends React.Component {
     return (
         <div>
             <h1>SQL Database</h1>
+            <div className='sql-outside'>
+            <h3>Table Name: 'titles'</h3>
+            {this.props.allSQLData.map(data => {
+                return (
+                    <p className='sql-divider'>Title: {data.title} <br /> Duration: {data.duration}</p>
+                )
+            })}
+            </div>
+            <div className='sql-outside2'>
+            <h3>Table Name: 'artists'</h3>
+            {this.props.allSQLData.map(data => {
+                return (
+                    <p className='sql-divider'>Artist: {data.artist} <br />Debut Year: {data.debutYear}</p>
+                )
+            })}
+            </div>
+            <div className='sql-outside3'>
+            <h3>Table Name: 'albums'</h3>
+            {this.props.allSQLData.map(data => {
+                return (
+                    <p className='sql-divider'>Album: {data.album} <br /> Year: {data.year}</p>
+                )
+            })}
+            </div>
             {this.props.allSQLData.map(data => {
                 return (
                     <SQLDataInfo key={data.title} title={data.title} duration={data.duration} 
